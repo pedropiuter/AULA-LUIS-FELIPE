43837a9e7da385e75a3d2cdb9d234cd0f5fb3383
alunoEtec <aula@etec.com>
2026-03-24 16:41:46 -0300

    salvando

--- server.js
+++ server.js
@@ -2,12 +2,36 @@
 //var express = require("express");
 
 import express, { response } from "express";
+import { request } from "express";
 
 const app = express();
 const porta = 3000;
 
 app.get("/",(request,response) => {
   response.send("Olá (^-^)")
+});
+
+app.get("/usuario", (request, response) => {
+    response.json({
+      id:1,
+      nome: "Pedro felipe",
+      profissao: "vagabundo"
+    });
+});
+
+app.get("/usuarios", (request, response) =>{
+  response.json([
+    {
+      id:1,
+      nome:"maria",
+      profissao:"mecanica"
+    },
+    {
+      id:2,
+      nome:"pedro",
+      profissao:"servente"
+    }
+  ]);
 });
 
 app.listen(porta, (request,response)=>{
